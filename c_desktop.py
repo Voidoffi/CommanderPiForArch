7de538c84788b5bad7f0d473fdd74de938c01f0b
Voidoffi <jonas.charrier1@gmail.com>
2022-12-11 10:23:08 +0100

    Update c_desktop.py

--- c_desktop.py
+++ c_desktop.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python
 import sys
 import os
-import subprocess as sp
 from os.path import expanduser
 
 user = sys.argv[1]
-home_path = expanduser("~%s" % user)
+home_path = expanduser(f"~{user}")
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 print(dir_path)
-f_content = "[Desktop Entry]\nName=CommanderPi\nComment=System info and overclocking\nExec="+dir_path+"/src/start.sh\nIcon="+dir_path+"/src/icons/Icon.png\nCategories=Utility;\nVersion=1.0\nType=Application\nTerminal=false\nStartupNotify=true"
+f_content = f"""[Desktop Entry]\n
+                Name=CommanderPi\n
+                Comment=System info and overclocking\n
+                Exec={dir_path}/src/start.sh\n
+                Icon={dir_path}/src/icons/Icon.png\n
+                Categories=Utility;\n
+                Version=1.0\n
+                Type=Application\n
+                Terminal=false\n
+                StartupNotify=true"""
 print(f_content)
 
 d_dir = home_path+"/Desktop/commanderpi.desktop"
 x_dir = "/usr/share/applications/commanderpi.desktop"
 
-print("Save desktop shortcut to %s" % d_dir)
+print(f"Save desktop shortcut to {d_dir}")
 try:
     f = open(d_dir, "w")
 except FileNotFoundError:
@@ -26,7 +34,7 @@ else:
         f.write(f_content)
 
 
-print("Save menu shortcut to %s" % x_dir)
+print(f"Save menu shortcut to {x_dir}")
 try:
     f2 = open(x_dir, "w")
 except FileNotFoundError:
